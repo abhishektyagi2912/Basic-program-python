c9944cb4d771eee9e20f7be777852bd9127e3788
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-05-15 13:22:10 +0530

    Add files via upload

---
+++ color text.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Abhishek Tyagi "+ Fore.YELLOW+ Back.BLUE+"I am student")
+print(Back.CYAN+"Hi My name is Abhishek Tyagi")
+print(Fore.RED + Back.GREEN+ "Hi My name is Abhishek Tyagi")
